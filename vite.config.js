6b84fbe4a6ad4a5ea9a0a3965af715ecef4574b6
Someleze Lucky mazwi <slmazwi@gmail.com>
2026-03-01 00:02:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+// We create our own __dirname because it's not available in modern ES Modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  logLevel: 'error', 
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
+      // This is the "magic" that tells Vite that @/ means the src folder
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "0.0.0.0",
-  }
-});
+})
